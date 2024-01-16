7201eb882d08e91d040ffcaecdc4661d61fce0d1
Saimum-14028 <md.amiralfahim@gmail.com>
2024-01-16 06:49:27 +0600

    404 page not found added alhamdlillah

--- src/Route.jsx
+++ src/Route.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
 
 const myCreatedRoute =  createBrowserRouter([
     {
         path : "/",
+        errorElement: <ErrorPage></ErrorPage>,
     }
 ])
-
 export default myCreatedRoute;
